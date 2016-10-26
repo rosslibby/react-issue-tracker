edf9e28e9aaf1ff77c48defb6e2b29fb6854c891
Ross Libby <libtheross@gmail.com>
2016-10-26 18:35:11 -0500

    added next/prev pagination

--- src/Components/GetData.js
+++ src/Components/GetData.js
@@ -5,18 +5,24 @@ import Title from './Title';
 class GetData extends Component {
     constructor (props) {
         super(props);
-        this.state = { items: [] };
+        this.state = { items: [], page: 0 };
     }
 
     componentDidMount () {
+        this.fetchData(1);
+    }
+
+    fetchData (numPages) {
+        numPages = this.state.page + numPages || 1;
+
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/repos/rails/rails/issues?per_page=25', true);
+        xhr.open('GET', 'https://api.github.com/repos/rails/rails/issues?per_page=25&page=' + numPages, true);
         xhr.setRequestHeader('Accept', 'application/vnd.github.text+json');
         xhr.send(null);
         xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
-                    this.setState({ items: JSON.parse(xhr.responseText)});
+                    this.setState({ items: JSON.parse(xhr.responseText), page: numPages });
                 }
             }
         };
@@ -35,39 +41,44 @@ class GetData extends Component {
 
     render () {
         return (
-            <ol>
-                {this.state.items.map((item) => {
+            <div>
+                <button onClick={() => {this.fetchData(-1)}}>{'Prev page'}</button>
+                <button onClick={() => {this.fetchData(1)}}>{'Next page'}</button>
+                <hr />
+                <ol>
+                    {this.state.items.map((item) => {
 
-                    const titleProps = {
-                        id: item.id,
-                        text: item.title
-                    };
+                        const titleProps = {
+                            id: item.id,
+                            text: item.title
+                        };
 
-                    return (
-                        <li key={ item.id }>
-                            <Title { ...titleProps } />
-                            <a href={ item.html_url } target="_blank">{'[link]'}</a>
-                            <p>{ item.body_text.substr(0, 140) }</p>
-                            <small><em>{ item.user.login }</em></small>
-                            <img src={ item.user.avatar_url + '&s=88' } alt={ item.user.login } />
-                            { item.labels.map((label, index) => {
+                        return (
+                            <li key={ item.id }>
+                                <Title { ...titleProps } />
+                                <a href={ item.html_url } target="_blank">{'[link]'}</a>
+                                <p>{ item.body_text.substr(0, 140) }</p>
+                                <small><em>{ item.user.login }</em></small>
+                                <img src={ item.user.avatar_url + '&s=88' } alt={ item.user.login } />
+                                { item.labels.map((label, index) => {
 
-                                const labelProps = {
-                                    key: index,
-                                    text: label.name,
-                                    url: this.formatLabel(label.url)
-                                };
+                                    const labelProps = {
+                                        key: index,
+                                        text: label.name,
+                                        url: this.formatLabel(label.url)
+                                    };
 
-                                return (
-                                    <Label
-                                        { ...labelProps }
-                                    />
-                                );
-                            }) }
-                        </li>
-                    );
-                })}
-            </ol>
+                                    return (
+                                        <Label
+                                            { ...labelProps }
+                                        />
+                                    );
+                                }) }
+                            </li>
+                        );
+                    })}
+                </ol>
+            </div>
         );
     }
 };
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GetData, Header, List } from './Components/';
+import { GetData, Header } from './Components/';
 import './index.css';
 
 ReactDOM.render(
     <div>
         <Header title="Rails: issues" />
         <GetData />
-        <List items={[{
-            text: 'alpha',
-            key: 1
-        }, {
-            text: 'beta',
-            key: 2
-        }]} />
     </div>,
   document.getElementById('root')
 );
