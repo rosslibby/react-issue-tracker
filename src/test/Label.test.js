05241cf03d8d9cb9fe590dc7151e6542a16af0cf
Ross Libby <libtheross@gmail.com>
2016-10-29 11:10:16 -0500

    Built label test

--- src/test/Label.test.js
+++ src/test/Label.test.js
@@ -7,5 +7,18 @@ it('should have a link', () => {
 
     let tree = label.toJSON();
 
-    expect(tree).toMatchSnapshot();
+    expect(tree.type).toEqual('a');
+});
+
+it('should reformat URL and render text from props', () => {
+    const data = {
+        text: 'Alpha',
+        url: 'https://api.github.com/repos/rails/rails/issues'
+    };
+    const label = renderer.create(<Label { ...data } />);
+
+    let tree = label.toJSON();
+
+    expect(tree.props.href).toEqual('https://github.com/rails/rails/issues');
+    expect(tree.children[0]).toEqual('Alpha');
 });
