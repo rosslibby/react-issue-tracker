464fc70d87b40a4b2700abfde28b54ac1b2d316a
Ross Libby <libtheross@gmail.com>
2016-10-29 01:24:56 -0500

    updated excerpt test

--- src/test/Excerpt.test.js
+++ src/test/Excerpt.test.js
@@ -5,7 +5,7 @@ import { Excerpt } from '../Components/';
 it('renders a <= 140 character excerpt', () => {
     const str = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non nunc quis metus porta elementum. Nunc consequat sollicitudin cursus. Aenean pulvinar dictum orci, fermentum efficitur risus suscipit sed. Donec sodales, nulla eu lacinia commodo, sapien tortor tincidunt felis, vel egestas justo urna imperdiet mi. Nulla condimentum enim et lorem elementum varius. Sed condimentum porta ipsum nec scelerisque. Quisque vitae tortor odio.';
     const excerpt = shallow(<Excerpt text={ str } />);
-    const p = <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non nunc quis metus porta elementum. Nunc consequat sollicitudin cursus....</p>;
+    const p = <p className="excerpt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non nunc quis metus porta elementum. Nunc consequat sollicitudin cursus....</p>;
 
     expect(excerpt.contains(p)).toEqual(true);
 });
