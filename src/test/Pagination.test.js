8711ac0d16e8c4f9e824facebaf0401273723f05
Ross Libby <libtheross@gmail.com>
2016-10-30 01:38:13 -0500

    Updated tests

--- src/test/Pagination.test.js
+++ src/test/Pagination.test.js
@@ -27,14 +27,14 @@ it('should render the following buttons: prev, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ne
 
     let tree = pages.toJSON();
 
-    expect(tree.children[0].children[0]).toEqual('Prev page');
+    expect(tree.children[0].children[0]).toEqual(String.fromCharCode('0x2039'));
     expect(tree.children.length).toEqual(12);
 
     for (var i = 1; i <= 10; i++) {
         expect(tree.children[i].children[0]).toEqual(i);
     }
 
-    expect(tree.children[11].children[0]).toEqual('Next page');
+    expect(tree.children[11].children[0]).toEqual(String.fromCharCode('0x203A'));
 });
 
 it('should have a click handler on each button', () => {
